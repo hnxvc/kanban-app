c6a8b7c6eba8f926ece97b61b5db1fa52eeddc7e
hoafelizz <hoa.nguyen@felizz.com>
2017-05-26 01:01:19 +0700

    Add behavior show or hide card details

--- src/components/KanbanBoard/List/Card/Card.js
+++ src/components/KanbanBoard/List/Card/Card.js
@@ -2,16 +2,33 @@ import React from 'react';
 import './Card.css';
 import PropTypes from 'prop-types';
 import CheckList from './CheckList/CheckList';
+import classNames from 'classnames';
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowDetails: true
+    }
+  }
   render() {
+    let titleClass = classNames(
+      'Card__title',
+      {'Card__title--is-open': this.state.isShowDetails}
+    );
     return(
       <div className="Card">
-        <h4 className="Card__title">{this.props.title}</h4>
-        <div className="Card__details">
-          <p className="Card__descrition">{this.props.description}</p>
-          <CheckList id={this.props.id} tasks={this.props.tasks}/>
-        </div>
+        <h4 className={titleClass}
+          onClick={()=> this.setState({isShowDetails: !this.state.isShowDetails})}
+          >{this.props.title}</h4>
+        {
+          this.state.isShowDetails &&
+          <div className="Card__details">
+            <p className="Card__descrition">{this.props.description}</p>
+            <CheckList id={this.props.id} tasks={this.props.tasks}/>
+          </div>
+        }
+
       </div>
     );
   }
