28ebec513f0753a07f7196f5570002ea2bb4233c
hoafelizz <hoa.nguyen@felizz.com>
2017-06-06 09:41:48 +0700

    Add edit card

---
+++ src/components/KanbanBoard/EditCard/EditCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import CardForm from '../CardForm/CardForm';
+import { Map } from 'immutable';
+import PropTypes from 'prop-types';
+
+class EditCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleChange(field, value) {
+    this.setState({
+      [field]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.cardCallbacks.updateCard(this.state);
+    this.props.history.pushState(null, '/');
+  }
+
+  handleCloseModal() {
+    this.props.history.pushState(null, '/');
+  }
+
+  componentWillMount() {
+    let card = this.props.cards.find(card => card.id === this.props.params.cardId);
+    this.setState({...card});
+  }
+
+  render() {
+    return(
+      <CardForm
+        draftCard={this.state}
+        buttonLabel="Create Card"
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        handleCloseModal={this.handleCloseModal}
+      />
+    );
+  }
+}
+
+EditCard.PropTypes = {
+  cardCallbacks: PropTypes.object
+}
+
+export default EditCard;
